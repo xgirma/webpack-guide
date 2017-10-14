01e75dc2580864bb20f41037d09cad98e93165c1
Girma Nigusse <xgirma@gmail.com>
2017-10-14 15:06:20 -0700

    bundle data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
-    var element = document.createElement('div');
+    let element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
-    var myIcon = new Image();
+    let myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
+
+    console.log(Data);
 
     return element;
 }
