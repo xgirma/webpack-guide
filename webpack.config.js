bc9e22c6a158be89c23e5257833f7194ed54e33b
Girma Nigusse <xgirma@gmail.com>
2017-10-22 16:06:03 -0700

    code spliting: webpackChunkName

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,18 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
     entry: {
         index: './src/index.js',
-        another: './src/another-module.js'
     },
     plugins: [
         new HTMLWebpackPlugin({
             title: 'Code Splitting'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common' //  Specify the common bundle's name
         })
     ],
     output: {
         filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
